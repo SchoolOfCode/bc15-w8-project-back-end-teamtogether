3a64392c672f983e53f7cd2ef4e74cc145703605
LuisValrod <luisrodriguezvalido@gmail.com>
2023-10-25 16:39:37 +0100

    test fail

--- routes/quiz.test.js
+++ routes/quiz.test.js
@@ -1,7 +1,7 @@
 import { test, expect, afterAll } from "vitest";
 import request from "supertest";
 import app from "../app.js";
-import { resetDatabase } from "../db/reset-database.js";
+import  { resetDatabase }  from "../db/reset-database.js";
 import { pool } from "../db/index.js";
 
 test("GET /questions works", async function () {
