0044bde1ea6f064e1be6015cf3719d851c6558ac
ZoeBrooks539 <zmbfood72@gmail.com>
2023-11-01 16:28:51 +0000

    host changes made

--- server.js
+++ server.js
@@ -1,7 +1,8 @@
 import { app } from "./app.js";
 
 const port = process.env.PORT ?? 3000;
+const host = process.env.HOST ?? "0.0.0.0";
 
-app.listen(port, () => {
+app.listen(port, host, () => {
   console.log(`Server is now listening on http://localhost:${port}`);
 });
